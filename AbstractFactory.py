e02ad5e776954cd50c1eea064bff93e310738f8d
daSugano <s_sugano@dreamarts.co.jp>
2022-08-08 10:44:15 +0900

    init

---
+++ AbstractFactory.py
@@ -0,0 +1,61 @@
+import abc
+import enum
+
+
+class AbstractFactory(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def cook(self):
+        return NotImplementedError
+
+
+class Food(metaclass=abc.ABCMeta):
+    def get_calorie(self) -> int:
+        return NotImplementedError
+
+
+class Carry(Food):
+    def __init__(self) -> None:
+        self.__calorie = 700
+
+    def get_calorie(self):
+        return self.__calorie
+
+
+class CarryFactory(AbstractFactory):
+    def cook(self):
+        carry = Carry()
+        return carry
+
+
+class Ramen(Food):
+    def __init__(self) -> None:
+        self.__calorie = 800
+
+    def get_calorie(self) -> int:
+        return self.__calorie
+
+
+class RamenFactory(AbstractFactory):
+    def cook(self):
+        ramen = Ramen()
+        return ramen
+
+
+class FoodType(enum.Enum):
+    RAMEN = "Ramen"
+    CARRY = "Carry"
+
+
+def getFactory(food: FoodType):
+    f = FoodType
+    food_dict = {
+        f.RAMEN.value: RamenFactory(),
+        f.CARRY.value: CarryFactory()
+    }
+
+    return food_dict.get(food)
+
+
+if __name__ == "__main__":
+    f = getFactory("Ramen")
+    print(f.cook())
